d434c8fc85b8742d454c4a2e7fc21803748d6c37
Gerhard Herbert <gerhard.herbert@smibe.com>
2017-09-28 18:33:38 +0200

    Some changes.

--- src/App.js
+++ src/App.js
@@ -6,28 +6,70 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: null
+      user: null,
+      view: "login"
     }
   }
   render() {
     return (
+      <div className='app'>
+  <header>
+    <div className="wrapper">
       <div>
-      <h1>Vokabeltrainer</h1>
+      <h1>Vokie - Vokabeltrainer</h1>
+      <ul>
+        <li><a className={this.state.view == "training" ? "active" : "view"}  href='#training' 
+            onClick={() =>  this.setState({view: "training"})}>Trainieren</a></li>
+        <li><a className={this.state.view == "lessons" ? "active" : "view"}  href='#lessons' 
+            onClick={() =>  this.setState({view: "lessons"})}>Lektionen</a></li>
+      </ul>
+      </div>
       {this.state.user ?
-        <button onClick={this.logout}>Abmelden</button>                
+        <img onClick={this.logout} className='user-profile'src={this.state.user.photoURL} />
         :
         <button onClick={this.login}>Anmelden</button>              
   } 
-  <div id="firebaseui-auth"></div>
   </div>
+  </header>
+  { this.state.view == "login"  ? 
+    <div style={{textAlign : 'center', backgroundColor: 'white'}}>
+      <div>Bitte erst anmelden</div>
+      <div id="firebaseui-auth" /> 
+    </div> : null }
+  
+  { this.state.view == "training" ? <div> Trainieren </div> : null }
+  { this.state.view == "lessons" 
+    ? 
+      <div> 
+        <div>
+        Lektionen
+        </div>
+        <button onClick={this.setState({view: "createLesson"})}>Neu</button> 
+      </div> 
+    : null }
+  { this.state.view == "createLesson" ? 
+  <div> 
+    <div>
+    Lektion erstellen
+    </div>
+    <button onClick={this.setState({view: "lessons"})}>Speichern</button> 
+  </div> : null }
+</div>
     );
   }
   componentDidMount = () => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user : user, view : user ? "training" : "login" });
+      } 
+    });
   }
+
 logout = () => {
     authUi.reset();
         this.setState({
-            user: null
+            user: null,
+            view: "login"
         })
     }
 
@@ -36,7 +78,8 @@ login = () => {
       'callbacks': {
         'signInSuccess': function(user) {
       this.setState({
-        user
+        user : user,
+        view : "training"
       });
           return false;
         }
